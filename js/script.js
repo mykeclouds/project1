951cd535b8a93c28543cc67f247c8622c38a8cbf
mykeclouds <piercingclouds@outlook.com>
2019-05-06 02:56:40 -0700

    final submit exceeds expectation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
-
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Aiming for Exceeds Expectations Grade
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-this is my array of quotes
-***/
+// this is my array of quotes
 const quotes = [ 
 
   {
@@ -38,61 +33,50 @@ const quotes = [
     quote : "Technology is changing at a ridiculous pace, and while you might feel like you’re the only person falling behind, the truth is no one person is on top of it all. ",
     source: "Emily Schweiss",
     year : 2017,
-    citation : "Slaying the Dragon: Imposter Syndrome"
+    citation : "Slaying the Dragon: Imposter Syndrome",
+    tag: "Inspire"
   },
   {
     quote : "If we could change ourselves, the tendencies in the world would also change. As a man changes his own nature, so does the attitude of the world change towards him. ... We need not wait to see what others do.",
     source: "Mohandas Gandhi",
-    year : 1913
+    year : 1913,
+    tag: "Inspire"
   },
   {
     quote : "I can accept failure, everyone fails at something. But I can’t accept not trying. ",
     source: "Michael Jordan ",
-    year : 1996
+    year : 1996,
+    tag: "Inspire"
   },
   {
     quote : "Remembering you are going to die is the best way I know to avoid the trap of thinking you have something to lose. You are already naked. There is no reason not to follow your heart. ",
     source: "Steve Jobs",
-    year : 2005
+    year : 2005,
+    tag: "Inspire"
   }
 ]
+
 // print quotes to log
 for (var i in quotes){
   console.log(i,": ", quotes[i]);
 }
 
-
-/***
- Create the `getRandomQuote` function to:
-  - Create a variable to
-  - Use the random number to `return` a random quote object from the `quotes` array. === randomQuote
-***/
-
+// this selects random quote from quotes array
 function getRandomQuote() {
   //store a random number as rand
 let rand = Math.floor(Math.random() * quotes.length);
 let randomQuote = quotes[rand];
-// returns randoly selected quote 
+// returns randomly selected quote 
 return  randomQuote;
-console.log(quotes.join(" ;"));
 }
-getRandomQuote();
 
-
-
-
-/***
-This function grab random quote
-Print to the screen
-Change thebackground color
-  - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-let html = " ";
-
+// This function grab a random quote and prints to the screen with new background color
 function printQuote() {
+  // reset html string to hold quote
+  let html = " ";
   let randQuote = getRandomQuote();
   let quote = randQuote.quote;
-  
+  // set quote printout
   html += '<p class="quote">'+ quote + "</p>";
   html += '<p class="source">' + randQuote.source ;
   //checks for the citation property 
@@ -108,27 +92,39 @@ function printQuote() {
       let year = randQuote.year;
       html += '<span class="year">' + year + "</span>";
     } 
- 
     // Adds tag to quote printout
   html += '<span class="citation">' + randQuote.tag + "</span>";
    //Closes printout
    html += " </p>";
 
 //changes backgorund color before printing quote
+  randomQuoteBackColor(); 
   randomBackgroundColor();
   print(html);
   }
 
-
-
-  function print(html) {
+function print(message) {
   let quoteBoxDiv = document.getElementById('quote-box');
-      quoteBoxDiv.innerHTML = html;
+      quoteBoxDiv.innerHTML = message;
   }
 
-
   // random background color generator
-function randomBackgroundColor() {
+function randomQuoteBackColor() {
+  //get red color
+  let r = Math.floor(Math.random() * 255);	
+  // get green color
+  let g = Math.floor(Math.random() * 255);
+  // get blue color
+  let b = Math.floor(Math.random() * 255);	
+  // set background color	varaible
+  let quoteColor = " rgb(" + r + ", " + g + ", " + b + ")";
+  /**  logs color to console
+   console.log(quoteColor);
+  **/
+  document.getElementById('quote-box').style.backgroundColor = quoteColor;
+     }
+
+  function randomBackgroundColor() {
   //get red color
   let r = Math.floor(Math.random() * 255);	
   // get green color
@@ -137,28 +133,22 @@ function randomBackgroundColor() {
   let b = Math.floor(Math.random() * 255);	
   // set background color	varaible
   let backColor = " rgb(" + r + ", " + g + ", " + b + ")";
-  console.log(backColor);
+    /**  logs color to console
+   console.log(backColor);
+  **/
   
-  document.getElementById('quote-box').style.backgroundColor = backColor;
+  document.body.style.backgroundColor = backColor;
      }
   
-
-function change() {
-  var intervalNum = setInterval(printQuote,10000);
-  }
   
+function change() {
+  var intervalNum = setInterval(printQuote,20000);
+  }
   
 change();
 
 
 /***
- When the "Show another quote" button is clicked, the event listener
- below will be triggered, and it will call, or "invoke", the `printQuote`
- function. So do not make any changes to the line of code below this
- comment.
+sets button to reprint quote
 ***/
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
